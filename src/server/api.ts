b0c69c1e36a6da17401ffc4a7280f5cc7d6ccb66
yehon <yehonatanrefael@gmail.com>
2025-11-08 20:20:01 +0200

    Add Postgres data provider for production environment

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,8 +1,15 @@
 import { remultApi } from 'remult/remult-express';
 import { List } from '../shared/list.ts';
 import { AuthController } from '../shared/AuthController.ts';
+import { createPostgresDataProvider } from 'remult/postgres';
 
 export const api = remultApi({
+    dataProvider:
+        process.env['NODE_ENV'] === 'production'
+            ? createPostgresDataProvider({
+                  connectionString: process.env.DATABASE_URL,
+              })
+            : undefined,
     entities: [List],
     controllers: [AuthController],
 });
